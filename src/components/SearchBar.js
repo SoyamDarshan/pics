0f561ec4b8b220d9649d4fa65e980226228dfd9a
Soyam Darshan Pattanaik <32511312+SoyamDarshan@users.noreply.github.com>
2019-06-27 00:05:43 +0530

    added search handler in app.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,27 +3,30 @@ import React from 'react';
 class SearchBar extends React.Component{
     state = { term: '' };
     /*
+    // Multiple ways to solve unresolved error
+    //1st Way
     onFormSubmit = (event) =>  {
         event.preventDefault();
         console.log(this.state.term);
     }*/
+
     onFormSubmit(event){
         event.preventDefault();
-        console.log(this.state.term);
+        this.props.onSubmit(this.state.term);
     }
-
-
+    
+ //   <form className="ui form" onSubmit={event => this.onFormSubmit(event)  /*2nd Way*/ }>
 
     render() {
         return (
         <div className="ui segment">
-            <form className="ui form" onSubmit={event => this.onFormSubmit(event)}>
+            <form className="ui form" onSubmit={event => this.onFormSubmit(event)  /*2nd Way*/ }>
                 <div className="field">
                     <label>Image Search</label>
                     <input 
                     type="text" 
                     value={this.state.term} 
-                    onChange={e => this.setState({term: e.target.value})}
+                    onChange={e => this.setState({ term: e.target.value })}
                     >
                     </input>
                 </div>
